e1a55a0628d4d80e313ec3ef350eff90c3638974
leandrazan <leandra.zanger@hhu.de>
2023-04-13 11:21:06 +0200

    update Readme

---
+++ R/eventData.R
@@ -0,0 +1,10 @@
+#' Example Event Data set
+#'
+#' Event Data from CL final 2012/2013, publicly available from StatsBomb
+#'
+#' @format ## `eventData`
+#' A data frame with 3,338 rows and 156 columns.
+#' The original event data was loaded with the `StatsBombR` package,
+#' and the `allclean` function has been called.
+#' @source <https://github.com/statsbomb/open-data>
+"eventData"
